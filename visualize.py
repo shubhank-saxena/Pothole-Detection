daffaa7f87a12f619975b48282e78950d95f5b63
Pradyum Gupta <30495649+Prady96@users.noreply.github.com>
2018-12-21 22:26:21 +0530

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -589,7 +589,7 @@ def save_image(image, image_name, boxes, masks, class_ids, scores, class_names, 
             draw.rectangle((x1, y1, x2, y2), outline=color)
 
         # Label
-        font = ImageFont.truetype('/content/Mask_RCNN/fonts/arial.ttf', 15)
+        font = ImageFont.truetype('/usr/share/fonts/truetype/ttf-bitstream-vera/Vera.ttf', 15)
         # font = ImageFont.truetype('OpenSans-Semibold.ttf', 15)
         draw.text((x1, y1), "%s %f" % (label, score), (255, 255, 255), font)
 
